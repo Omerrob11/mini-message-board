13b76828e54ac7bfdc1149b30b249c4ec5007fe8
Omer Miloh <omerrob11@gmail.com>
2024-10-16 11:16:25 +0300

    chore: structure the project

---
+++ index.js
@@ -0,0 +1,16 @@
+// routes for users, for example
+// folder for the routers
+// we don't want to work with the data here
+// we import the controller
+// and use those function as handlers to our http methods
+
+// models:
+// where is the database most of the time.
+
+// controllers:
+// execute what you want to do with the data - manage it
+// we write the function that do stuff with the data
+// then export it
+
+// views:
+//client side - frontend

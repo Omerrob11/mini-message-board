f26566da77c9d62ac1f2c562a93bdb005696de3d
Omer Miloh <omerrob11@gmail.com>
2024-10-16 11:24:28 +0300

    feat: add messages dummy array

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+// orgnaize routes into groups, for each path?
+// so we extract the group of routes to their own file
+
+const { Router } = require("express");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
